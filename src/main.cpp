e7b2326de932166f66deb68393ad27267b3f44ba
Tymur Kramar <tim4ig2@gmail.com>
2025-07-03 04:04:31 +0200

    test

--- src/main.cpp
+++ src/main.cpp
@@ -5,16 +5,28 @@
 
 int main()
 {
-    auto a = t::big::BigInt<8192>(16);
-    const auto b = t::big::BigInt<8192>(16);
+    auto a = t::big::BigInt<8192>(17);
+    const auto b = t::big::BigInt<8192>(11);
 
     for (int i = 0; i < 350; ++i)
     {
-        std::cout << "i=" << i << ",val=" << a.to_string(t::big::BigIntBase::Hex) << '\n';
         a *= b;
     }
 
     std::cout << a.to_string() << '\n';
+
+    auto c = t::big::BigInt<8192>(432);
+    const auto d = t::big::BigInt<8192>(15);
+
+    for (int i = 0; i < 117; ++i)
+    {
+        c *= d;
+    }
+
+    std::cout << c.to_string() << '\n';
+
+    std::cout << (a + c).to_string() << '\n';
+
 
     return 0;
 }
